f21b8ee5dfdd0895fd6a7a9402d74d26c6e5b15a
Jassu-590 <135200245+Jassu-590@users.noreply.github.com>
2023-11-25 15:57:15 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+class RegistrationForm(models.Model):
+  username=models.EmailField()
+  password=models.CharField(max_length=20)
+  cpassword=models.CharField(max_length=20)  
+class LoginForm(models.Model):
+  Username=models.EmailField()
+  password=models.CharField(max_length=50)
+class Product(models.Model):
+  pname=models.CharField(max_length=50)
+  pcost=models.FloatField()
+  pdetails=models.CharField(max_length=100)
+  cat=models.IntegerField()
+  is_active=models.BooleanField(default=True)
+  pimage=models.ImageField(upload_to="image" )
+  def __str__(self):
+    return self.pname
+
+class Cart(models.Model):
+  uid=models.ForeignKey(User,on_delete=models.CASCADE,db_column="uid")
+  pid=models.ForeignKey(Product,on_delete=models.CASCADE,db_column="pid")
+  qty=models.IntegerField(default=1)
+
+class Order(models.Model):
+  order_id=models.CharField(max_length=50)
+  uid=models.ForeignKey(User,on_delete=models.CASCADE,db_column="uid")
+  pid=models.ForeignKey(Product,on_delete=models.CASCADE,db_column="pid")
+  qty=models.IntegerField(default=1)
